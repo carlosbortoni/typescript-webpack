a90cdc21932d5c4836ac182ddf89325b0f4586e2
Carlos Bortoni <carlosbortoni@gmail.com>
2020-06-27 00:19:37 -0300

    adicionando o live-reload

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import Livro from './modelo/livro';
 
-const livro = new Livro('Livro 1', 109.80, 0.10)
+const livro = new Livro('TypeScript Puro', 109.80, 0.10)
 // console.log(livro.precoComDesconto());
 
 $('body').append(`<h1>${livro.nome}</h1>`);
